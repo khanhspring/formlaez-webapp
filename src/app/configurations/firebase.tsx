8f1bf15ddf54cf629af336696592e5a012f23af0
khanh.tran <xuankhanhyb@gmail.com>
2023-06-01 22:53:06 +0700

    Update env

--- src/app/configurations/firebase.tsx
+++ src/app/configurations/firebase.tsx
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { Auth, getAuth, getIdToken, onAuthStateChanged } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIU3hF6Ef7sllY_ui_fOQs0W4ZoCKM8gM",
-  authDomain: "formini-so.firebaseapp.com",
-  projectId: "formini-so",
-  storageBucket: "formini-so.appspot.com",
-  messagingSenderId: "919693199622",
-  appId: "1:919693199622:web:6fc9ee69215e031850b687",
-  measurementId: "G-6CZGW1FPQF"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
